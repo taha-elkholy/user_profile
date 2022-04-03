4a71fa5badb691972294c2e26414e72ae2e9de62
taha <tahaelkholy.dev@gmail.com>
2022-04-03 11:46:09 +0200

    Add Custom Pink Material Color as in the dribble ui for app theme

--- lib/core/style/colors.dart
+++ lib/core/style/colors.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 Map<int, Color> _color =
 const {
-  50:Color.fromRGBO(136,14,79, .1),
-  100:Color.fromRGBO(136,14,79, .2),
-  200:Color.fromRGBO(136,14,79, .3),
-  300:Color.fromRGBO(136,14,79, .4),
-  400:Color.fromRGBO(136,14,79, .5),
-  500:Color.fromRGBO(136,14,79, .6),
-  600:Color.fromRGBO(136,14,79, .7),
-  700:Color.fromRGBO(136,14,79, .8),
-  800:Color.fromRGBO(136,14,79, .9),
-  900:Color.fromRGBO(136,14,79, 1),
+  50:Color.fromRGBO(224,77,91, .1),
+  100:Color.fromRGBO(224,77,91, .2),
+  200:Color.fromRGBO(224,77,91, .3),
+  300:Color.fromRGBO(224,77,91, .4),
+  400:Color.fromRGBO(224,77,91, .5),
+  500:Color.fromRGBO(224,77,91, .6),
+  600:Color.fromRGBO(224,77,91, .7),
+  700:Color.fromRGBO(224,77,91, .8),
+  800:Color.fromRGBO(224,77,91, .9),
+  900:Color.fromRGBO(224,77,91, 1),
 };
 
 MaterialColor customPink = MaterialColor(0xFFE04D5B, _color);
